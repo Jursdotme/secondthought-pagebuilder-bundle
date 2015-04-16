267c42aea8864ef742e5ca100a4bc1750e86931d
Rasmus Jürs <rasmus@jurs.me>
2015-04-16 10:46:13 +0200

    Add feature widget

---
+++ secondthought-widgets/secondthought-feature-widget/secondthought-feature-widget.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+Widget Name: Secondthought Feature Widget
+Description: Add slideshows to any page you want.
+Author: Me
+Author URI: http://example.com
+*/
+
+class secondthought_feature_widget extends SiteOrigin_Widget {
+	function __construct() {
+
+		parent::__construct(
+			'secondthought-feature-widget',
+			__('Secondthought Feature Widget', 'hello-world-widget-text-domain'),
+			array(
+				'description' => __('A simple feature widget.', 'hello-world-widget-text-domain'),
+			),
+			array(),
+			array(
+				'header_text' => array(
+	        'type' => 'text',
+	        'label' => __('Header Text', 'widget-form-fields-text-domain'),
+	        'default' => ''
+		    ),
+				'label_text' => array(
+	        'type' => 'text',
+	        'label' => __('Caption Text', 'widget-form-fields-text-domain'),
+	        'default' => ''
+		    ),
+				'link' => array(
+	        'type' => 'text',
+	        'label' => __('Link', 'widget-form-fields-text-domain'),
+	        'default' => ''
+		    ),
+				'text_placement' => array(
+	        'type' => 'radio',
+	        'label' => __( 'Text placement', 'widget-form-fields-text-domain' ),
+	        'default' => 'below',
+	        'options' => array(
+            'above' => __( 'Above image', 'widget-form-fields-text-domain' ),
+            'below' => __( 'Below image', 'widget-form-fields-text-domain' ),
+	        )
+		    ),
+				'image' => array(
+		        'type' => 'media',
+		        'label' => __( 'Image', 'widget-form-fields-text-domain' ),
+		        'choose' => __( 'Choose image', 'widget-form-fields-text-domain' ),
+		        'update' => __( 'Set image', 'widget-form-fields-text-domain' ),
+		        'library' => 'image'
+		    )
+			),
+			plugin_dir_path(__FILE__)
+		);
+	}
+
+	function get_template_name($instance) {
+		return 'secondthought-feature-widget-template';
+	}
+
+	function get_style_name($instance) {
+		return 'secondthought-feature-widget-style';
+	}
+
+	/**
+	 * Enqueue the feature scripts
+	 */
+	function enqueue_frontend_scripts(){
+		wp_enqueue_script('secondthought-feature-widget');
+	}
+
+}
+
+siteorigin_widget_register('secondthought-feature-widget', __FILE__, 'Secondthought_feature_widget');
+
+/**
+ * Register all the feature scripts
+ */
+function secondthought_feature_register_scripts(){
+
+	wp_register_script('secondthought-feature-widget', siteorigin_widget_get_plugin_dir_url('secondthought-feature-widget').'js/secondthought-feature-widget.js', array('jquery'), SOW_BUNDLE_VERSION);
+}
+add_action('wp_enqueue_scripts', 'secondthought_feature_register_scripts', 1);
---
+++ secondthought-widgets/secondthought-feature-widget/tpl/secondthought-feature-widget-template.php
@@ -0,0 +1,46 @@
+<div class="secondthought-feature-item">
+
+  <?php
+    // Setup Image data
+    $id = $instance['image'];
+    $size = 'medium'; // (thumbnail, medium, large, full or custom size)
+    $image_attributes = wp_get_attachment_image_src( $id, $size );
+  ?>
+  <a href="<?php echo $instance['link']; ?>">
+
+    <?php if ($instance['text_placement'] == "above"): ?>
+
+      <div class="secondthought-feature-caption">
+
+        <?php if ($instance['header_text']): ?>
+          <h4><?php echo $instance['header_text']; ?></h4>
+        <?php endif; ?>
+
+        <?php if ($instance['label_text']): ?>
+          <p><?php echo $instance['label_text']; ?></p>
+        <?php endif; ?>
+
+      </div>
+
+    <?php endif; ?>
+
+    <img src="<?php echo $image_attributes[0]; ?>" width="<?php echo $image_attributes[1]; ?>" height="<?php echo $image_attributes[2]; ?>">
+
+    <?php if ($instance['text_placement'] == "below"): ?>
+
+      <div class="secondthought-feature-caption">
+
+        <?php if ($instance['header_text']): ?>
+          <h4><?php echo $instance['header_text']; ?></h4>
+        <?php endif; ?>
+
+        <?php if ($instance['label_text']): ?>
+          <p><?php echo $instance['label_text']; ?></p>
+        <?php endif; ?>
+
+      </div>
+
+    <?php endif; ?>
+
+  </a>
+</div>
